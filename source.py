e08cb9d0062a8d2ca9f6193b018b4db6d93d375a
Arye Ludmir <azev83@gmail.com>
2021-04-20 23:06:54 -0400

    Finished setting up

---
+++ source.py
@@ -0,0 +1,4 @@
+# This file is required. Use the same name, "source.py".
+# All of your *foundational* code goes here, meaning the functions and classes
+# that can be used to build larger processes. For example, the class you
+# created for the OOP assignment would go here.
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-# This is where they test you...
+# This file is required. Use the same name, "test.py". Below you see an example
+# of importing a class from "source.py", instantiating a new object and printing
+# that object. Replace the code below with your own.
