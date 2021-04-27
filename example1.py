57fb4b23c79c2505ac929cf638f0144e523a345a
Arye Ludmir <azev83@gmail.com>
2021-04-27 19:24:31 -0400

    preliminary setup complete. (simple example file)

---
+++ example1.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from source import WallFooting, ColumnFooting
+
+df = pd.read_csv("example1.csv")
+print(df.head())
+
+for index, row in df.iterrows():
+
+    ftng_type = row["ftng_type"]
+    width = row["width"]
+    d_l = row["dead_load"]
+    l_l = row["live_load"]
+    conc_type = row["conc_type"]
+    w_c = row["w_c"]
+    w_e = row["w_e"]
+    f_c = row["f_c"]
+    f_y = row["f_y"]
+    a_s_p = row["a_s_p"]
+    bottom = row["bottom_of_ftng"]
+    bar_coat = row["bar_coat"]
+
+    if ftng_type == "wall":
+        wall_type = row["wall_type"]
+
+        footing = WallFooting(
+            width,
+            wall_type,
+            d_l,
+            l_l,
+            f_c,
+            f_y,
+            a_s_p,
+            bottom,
+            bar_coat,
+            conc_type,
+            w_c,
+            w_e,
+        )
+
+    else:
+        max_width = row["width_restriction"]
+        col_loc = row["col_loc"]
+
+        footing = ColumnFooting(
+            width,
+            d_l,
+            l_l,
+            f_c,
+            f_y,
+            a_s_p,
+            bottom,
+            max_width,
+            bar_coat,
+            col_loc,
+            conc_type,
+            w_c,
+            w_e,
+        )
+
+    print(footing)
--- source.py
+++ source.py
@@ -1,43 +1,109 @@
 import math
 
 
-def factor_loads(self, d_l, d_d):
-    """ Calculates the minimum design load by multiplying service loads by given factors (ACI Section 5.3.1)
-    NOTE: This ACI section is slightly modified for our purpose. See ACI Code, Section 5.3 for more details.
-    """
+class Footing:
+    def __init__(
+        self,
+        d_l,
+        l_l,
+        f_c,
+        f_y,
+        a_s_p,
+        bottom,
+        bar_coat,
+        conc_type,
+        w_c,
+        w_e,
+    ):
+        self.d_l = d_l
+        self.l_l = l_l
+        self.f_c = f_c
+        self.f_y = f_y
+        self.a_s_p = a_s_p
+        self.bottom = bottom
+        self.bar_coat = bar_coat
+        self.conc_type = conc_type
+        self.w_c = w_c
+        self.w_e = w_e
 
-    return (1.6 * d_l) + (1.2 * d_d)
+    def factor_loads(self, d_l, d_d):
+        """Calculates the minimum design load by multiplying service loads by given factors (ACI Section 5.3.1)
+        NOTE: This ACI section is slightly modified for our purpose. See ACI Code, Section 5.3 for more details.
+        """
+
+        return (1.6 * d_l) + (1.2 * d_d)
+
+    def reqd_area(self, rho, b, d):
+        """Calculates the minimum flexural reinforcement required by analysis (ACI Section 9.6.1.1)
+
+        rho = ratio of tension steel area to effective concrete area
+        b = section width
+        d = effective depth of steel
+        """
+
+        return rho * b * d
+
+    def min_area_beams(self, concrete_strength, steel_strength, b, d):
+        """Calculates the minimum flexural reinforcement for nonprestressed beams
+        as required by ACI Section 9.6.1.2
+        """
+        a = (3 * math.sqrt(concrete_strength) / steel_strength) * b * d
+        b = (200 / steel_strength) * b * d
+        result = max(a, b)
+
+        print(
+            "ACI Section 9.6.1.2 - Minimum flexural reinforcement for nonprestressed beams: ",
+            result,
+        )
+
+        return result
+
+    def net_asp(self, asp, w_e, w_c, h, bottom):
+        return asp - (w_e * (bottom - h)) - (w_c * h)
 
 
-def reqd_area(self, rho, b, d):
-    """ Calculates the minimum flexural reinforcement required by analysis (ACI Section 9.6.1.1)
+class WallFooting(Footing):
+    def __init__(
+        self,
+        width,
+        wall_type,
+        d_l,
+        l_l,
+        f_c,
+        f_y,
+        a_s_p,
+        bottom,
+        bar_coat=None,
+        conc_type="nw",
+        w_c=150,
+        w_e=100,
+    ):
+        pass
+        #super.__init__()
 
-    rho = ratio of tension steel area to effective concrete area
-    b = section width
-    d = effective depth of steel
-    """
-
-    return rho * b * d
-
-def min_area_beams(self, concrete_strength, steel_strength, b, d):
-    """ Calculates the minimum flexural reinforcement for nonprestressed beams
-    as required by ACI Section 9.6.1.2
-    """
-    a = (3 * math.sqrt(concrete_strength) / steel_strength) * b * d
-    b = (200 / steel_strength) * b * d
-    result = max(a, b)
-
-    print('ACI Section 9.6.1.2 - Minimum flexural reinforcement for nonprestressed beams: ', result)
-
-    return result
-
-def net_asp(self, asp, w_e, w_c, h, bottom):
-    return asp - (w_e * (bottom - h)) - (w_c * h)
+    def design_wall_footing():
+        pass
 
 
-class TestClass:
-    def __init__(self, number):
-        self.number = number
+class ColumnFooting(Footing):
+    def __init__(
+        self,
+        width,
+        d_l,
+        l_l,
+        f_c,
+        f_y,
+        a_s_p,
+        bottom,
+        width_restriction=None,
+        bar_coat=None,
+        col_loc="center",
+        conc_type="nw",
+        w_c=150,
+        w_e=100,
+    ):
+        pass
+        #super.__init__()
 
-    def __str__(self):
-        return 'number: {}'.format(self.number)
+    def design_column_footing():
+        pass
